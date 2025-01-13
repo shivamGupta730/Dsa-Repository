798e485ac548e3f6ab59ac16261977a4f65d2e11
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-13 09:29:42 +0530

    initial commit

--- DP/fibonaciiNumber.java
+++ DP/fibonaciiNumber.java
@@ -3,6 +3,7 @@ package DP;
 import java.util.Scanner;
 
 public class fibonaciiNumber {
+    static  int[]dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number");
@@ -10,8 +11,17 @@ public class fibonaciiNumber {
         int ans=fib(n);
         System.out.println("your answer "+ans);
     }
-    public static int fib(int n) {
+    public static  int fibo(int n){
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+//         to check the dp array if it contains value
+        if(dp[n]!=0)return dp[n];
+        int ans=fibo(n-1)+fibo(n-2);
+//        add the ans into dp array
+        dp[n]=ans;
+        return ans ;
+    }
+    public static int fib(int n) {
+       dp=new int[n+1];// index from 0 to n
+       return fibo(n);
     }
 }

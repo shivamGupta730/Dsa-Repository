66d62c43f2848fb541f83bf2ecdf8804e2fb0b5f
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-16 19:02:40 +0530

    initial commit

---
+++ Stacks/Basic.java
@@ -0,0 +1,22 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class Basic {
+    public static void main(String[] args) {
+        Stack<Integer>st=new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+
+        System.out.println(st.peek());
+        System.out.println(st);
+        System.out.println(st.pop());
+        System.out.println(st);
+        System.out.println(st.isEmpty());
+        System.out.println(st.size());
+
+    }
+}

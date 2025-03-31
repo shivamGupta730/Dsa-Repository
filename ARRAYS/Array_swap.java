a08b66c4ba179096e3d51c91abd72b2b36ff509c
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-03-31 16:28:34 +0530

    initial commit

---
+++ ARRAYS/Array_swap.java
@@ -0,0 +1,13 @@
+package ARRAYS;
+
+public class Array_swap {
+    public static void main(String[] args) {
+       int[]arr={2,1,3,4,5};
+       
+    }
+    public static void display(int[]arr){
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}

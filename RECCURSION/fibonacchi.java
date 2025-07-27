68bc2c3f8e4eabbcff24a101655ba771994967ce
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-07-27 16:37:22 +0530

    initial commit

--- RECCURSION/fibonacchi.java
+++ RECCURSION/fibonacchi.java
@@ -3,13 +3,35 @@ package RECCURSION;
 import java.util.Scanner;
 
 public class fibonacchi {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(print(n));
-    }
-    public static int  print(int n){
-       if(n==0||n==1) return n;
-       return print(n-1)+print(n-2);
+//    public static void main(String[] args) {
+//        Scanner sc= new Scanner(System.in);
+//        int n=sc.nextInt();
+//        System.out.println(print(n));
+//    }
+//    public static int  print(int n){
+//       if(n==0||n==1) return n;
+//       return print(n-1)+print(n-2);
+//    }
+static int fib(int n)
+{
+    // Base Case
+    if (n <= 1)
+        return n;
+
+    // Recursive call
+    return fib(n - 1) + fib(n - 2);
+}
+
+    // Driver Code
+    public static void main(String args[])
+    {
+        // Given Number N
+        int N = 10;
+
+        // Print the first N numbers
+        for (int i = 0; i < N; i++) {
+
+            System.out.print(fib(i) + " ");
+        }
     }
 }

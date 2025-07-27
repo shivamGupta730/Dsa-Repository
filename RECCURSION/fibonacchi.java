fb349a113245f92f7df6e750d417a63993a91c4a
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-07-27 16:39:22 +0530

    initial commit

--- RECCURSION/fibonacchi.java
+++ RECCURSION/fibonacchi.java
@@ -3,6 +3,7 @@ package RECCURSION;
 import java.util.Scanner;
 
 public class fibonacchi {
+//    first method
 //    public static void main(String[] args) {
 //        Scanner sc= new Scanner(System.in);
 //        int n=sc.nextInt();
